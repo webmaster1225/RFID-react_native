a479d147eea8d641cf088aa02ddbf6eb42ab8d75
Hardy Smith <hardysmith940110@gmail.com>
2018-08-31 15:38:33 -0500

    Updates to IOS

--- ios/NFCHelper.swift
+++ ios/NFCHelper.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreNFC
 
 class NFCHelper: NSObject {
-
+  
   //getFormatName:: NFCTypeNameFormat -> String
   func getFormatName (_ format: NFCTypeNameFormat) -> String {
     switch format {
@@ -29,27 +29,27 @@ class NFCHelper: NSObject {
       return "Unknown"
     }
   }
-
+  
   func getCode (_ char: Character) -> Bool {
     let charArray: [Character] = ["Q", "\u{01}", "&", "/", "(", ")"]
     return !charArray.contains(char)
   }
-
+  
   // formatData:: [NFCNDEFMessage] -> [[String: String]]
   @available(iOS 11.0, *)
   func formatData (_ nfcMessages: [NFCNDEFMessage]) -> [String: Any] {
     var format = ""
+    var nfcMsg:[String:Any] = [:]
+    
     let nfcDataMsg = nfcMessages.map({ (message) -> [[String: String]] in
       let records = message.records
       return records.map({ (payload) -> [String: String] in
         var nfcData: [String: String] = [:]
+        let payloadD = String(data: payload.payload.advanced(by: 3), encoding: String.Encoding.utf8 ) as String?
+        let encoding = String(data: payload.payload.advanced(by: 1).prefix(through: 1), encoding: String.Encoding.utf8 ) as String?
 
         format = getFormatName(payload.typeNameFormat)
-        let payloadD = String(data: payload.payload.advanced(by: 3), encoding: String.Encoding.utf8 ) as String!
-        let type = String(data: payload.type, encoding: String.Encoding.utf8 ) as String!
-        let identifier = String(data: payload.identifier, encoding: String.Encoding.utf8 ) as String!
-        let encoding = String(data: payload.payload.advanced(by: 1).prefix(through: 1), encoding: String.Encoding.utf8 ) as String!
-
+        
         nfcData["locale"] = encoding
         nfcData["encoding"] = "UTF-8"
         nfcData["type"] = "TEXT"
@@ -57,15 +57,14 @@ class NFCHelper: NSObject {
         return nfcData
       })
     }).flatMap { $0 }
-
-    var nfcMsg:[String:Any] = [:]
+    
     nfcMsg["origin"] = "ios"
     nfcMsg["data"] = [nfcDataMsg]
     nfcMsg["id"] = "unavailable"
     nfcMsg["type"] = format
     return nfcMsg
   }
-
+  
   // formatError:: Error -> [String: String]
   func formatError (_ error: Error) -> [String: String] {
     var errorObj: [String: String] = [:]
--- ios/ReactNativeNFC.swift
+++ ios/ReactNativeNFC.swift
@@ -10,9 +10,13 @@ import CoreNFC
 
 @objc(ReactNativeNFC)
 class ReactNativeNFC: RCTEventEmitter, NFCNDEFReaderSessionDelegate {
-
+  
   let nfcHelper = NFCHelper()
-
+  
+  @objc override static func requiresMainQueueSetup() -> Bool {
+    return true;
+  }
+  
   @objc func isSupported() -> Void {
     if #available(iOS 11.0, *) {
       if NFCNDEFReaderSession.readingAvailable {
@@ -25,7 +29,7 @@ class ReactNativeNFC: RCTEventEmitter, NFCNDEFReaderSessionDelegate {
     }
     sendNFCUnavailableEvent({})
   }
-
+  
   @objc func initialize() -> Void {
     if #available(iOS 11.0, *) {
       if NFCNDEFReaderSession.readingAvailable {
@@ -38,37 +42,37 @@ class ReactNativeNFC: RCTEventEmitter, NFCNDEFReaderSessionDelegate {
       sendNFCUnavailableEvent({})
     }
   }
-
+  
   override func supportedEvents() -> [String]! {
     return ["__NFC_DISCOVERED", "__NFC_ERROR", "__NFC_MISSING", "__NFC_UNAVAILABLE", "__NFC_ENABLED"]
   }
-
+  
   func sendEvent(_ data: Any) -> Void {
     sendEvent(withName: "__NFC_DISCOVERED", body: data)
   }
-
+  
   func sendErrorEvent(_ data: Any) -> Void {
     sendEvent(withName: "__NFC_ERROR", body: data)
   }
-
+  
   func sendNFCEnabledEvent(_ data: Any) -> Void {
     sendEvent(withName: "__NFC_ENABLED", body: data)
   }
-
+  
   func sendNFCUnavailableEvent(_ data: Any) -> Void {
     sendEvent(withName: "__NFC_UNAVAILABLE", body: data)
   }
-
+  
   func sendNFCMissingEvent(_ data: Any) -> Void {
     sendEvent(withName: "__NFC_MISSING", body: data)
   }
-
+  
   @available(iOS 11.0, *)
   func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) -> Void {
     let data = nfcHelper.formatError(error)
     sendErrorEvent(data)
   }
-
+  
   @available(iOS 11.0, *)
   func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) -> Void {
     let data = nfcHelper.formatData(messages)
